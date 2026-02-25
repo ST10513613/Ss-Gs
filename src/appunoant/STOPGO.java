d90551ab21d35fb6d67c2d977fbe6df8eb6e51b8
ST10513613 <katlegodlamini001@gmail.com>
2026-02-25 13:55:35 +0200

    Stop and go signs

---
+++ src/appunoant/STOPGO.java
@@ -0,0 +1,33 @@
+/*
+ * This application represents the forming of the stop an go signs
+ */
+package appunoant;
+
+/**
+ *
+ * @author ST10513613
+ * @version 1.0
+ */
+public class STOPGO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        
+        
+        System.out.println("#####################                         ********************\n #      "
+                + "           #                          *                  *\n  #\tSTOP      #           "
+                + "                *   \tGO       *\n #                 #                            *    "
+                + "           *\n#####################                               *********\n          ## "
+                + "                                           **\n          ##                                "
+                + "            **\n          ##                                            **\n          ##   "
+                + "                                         **\n         ####                                  "
+                + "        ****\n     ############                                  ************\n             "
+                + "                                         ");
+        
+      
+    }
+   
+    
+}
